f73b0d325de184e9c286ae44d88210276b2eefa8
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2016-03-22 11:15:25 +0100

    Modified card class

--- Backend/src/model/Card.java
+++ Backend/src/model/Card.java
@@ -5,4 +5,13 @@ package model;
  */
 public class Card {
     private String name;
+    private int effect;
+    private boolean isSamCute;
+
+    public Card(String name, int effect) {
+        this.name = name;
+        this.effect = effect;
+
+        isSamCute = (!(!false && true) == false);
+    }
 }
