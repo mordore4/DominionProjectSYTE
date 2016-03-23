6a7600e710ab97de15587365d036825739bb1ddd
YannisDejonghe <yannis.dejonghe@hotmail.com>
2016-03-23 11:06:30 +0100

    jquery loading screen time fit
    JQUERY LOADING SCREEN WITH ACTIVE TIME ADDED

--- Backend/web/assets/js/script.js
+++ Backend/web/assets/js/script.js
@@ -2,6 +2,7 @@
  * Created by Yannis on 1-12-2015.
  */
 
+var active = false;
 
 
 $(document).ready(function () {
@@ -16,25 +17,40 @@ $body = $("body");
 
 $(document).on({
     ajaxStart: function () {
-        $body.addClass("loading");
+        if (active) $body.addClass("loading");
     },
     ajaxStop: function () {
-        setTimeout(stopLoading, 1500);
+        if (active) setTimeout(stopLoading, 2000);
+        active = false;
     }
 });
 
-var stopLoading = function() {
+var stopLoading = function () {
     $body.removeClass("loading");
 };
 
-var playGame = function() {
+var playGame = function (e) {
+    e.preventDefault();
+
+    /*active = true;*/
+
     $.ajax({
         url: "index.html",
         method: "GET",
         data: "",
         dataType: "html",
-        success: function(data) {
+        success: function (data) {
             console.log(data);
         }
     })
+    $('#menu').hide();
+
+   /* var goTo = this.getAttribute("href");
+
+    window.location = goTo;
+
+
+    setTimeout(function () {
+        window.location = goTo;
+    }, 3000);*/
 };
