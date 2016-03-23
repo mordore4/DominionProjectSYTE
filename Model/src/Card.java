653f65684d47685bb5c4d532eaf82102f3bbe8ec
Sam <sam.demeulenaere@student.howest.be>
2016-03-23 14:28:12 +0100

    Added 5 classes
    Card, DiscardPile, Hand, Player, TrashPile

---
+++ Model/src/Card.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Sam on 23/03/2016.
+ */
+public class Card {
+    private String name;
+    private int[] abilities; //possibly wrong, Abilities might become class
+    private int type;
+    private int cost;
+    //private int count; Amount still buyable, probably not the right class
+}
---
+++ Model/src/Hand.java
@@ -0,0 +1,31 @@
+/**
+ * Created by Sam on 23/03/2016.
+ */
+public class Hand {
+    private Card[] cards;
+
+    public void build()
+    {
+
+    }
+
+    public void discard()
+    {
+
+    }
+
+    public void trash()
+    {
+
+    }
+
+    public void gain()
+    {
+
+    }
+
+    public void reveal()
+    {
+
+    }
+}
