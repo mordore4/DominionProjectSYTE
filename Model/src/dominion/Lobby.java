316f016367a77f70505fbcd18ddfcdbe26473e1a
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2016-04-14 11:42:25 +0200

    Added players to lobby

--- Model/src/dominion/Lobby.java
+++ Model/src/dominion/Lobby.java
@@ -1,8 +1,28 @@
 package dominion;
 
+import java.util.ArrayList;
+
 /**
  * Created by Digaly on 24/03/2016.
  */
 public class Lobby
 {
+    private ArrayList<Account> playersInLobby;
+
+
+    public Lobby()
+    {
+
+    }
+
+    public void addPlayer(Account account)
+    {
+        if (playersInLobby.size() < 4)
+        {
+            playersInLobby.add(account);
+        }
+    }
+
+
+
 }
