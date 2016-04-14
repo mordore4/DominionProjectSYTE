cebadce6d087e64d92eda9ca615fefdaa3dcba7f
Sam <sam.demeulenaere@student.howest.be>
2016-04-14 15:54:09 +0200

    Added stuff

--- Model/src/dominion/Account.java
+++ Model/src/dominion/Account.java
@@ -9,6 +9,13 @@ public class Account
     private int rankingScore;
     private String[] friendList;
 
+    public Account(String name, int rankingScore)
+    {
+        this.name = name;
+        this.rankingScore = rankingScore;
+        friendList = null;
+    }
+
     public String getName()
     {
         return name;
--- Model/src/dominion/Lobby.java
+++ Model/src/dominion/Lobby.java
@@ -9,11 +9,15 @@ public class Lobby
 {
     private ArrayList<Account> playersInLobby;
     private Game game;
+    private String name;
+    private String password;
 
 
-    public Lobby()
+    public Lobby(Account account, String name, String password)
     {
-
+        playersInLobby.add(account);
+        this.name = name;
+        this.password = password;
     }
 
     public void addPlayer(Account account)
