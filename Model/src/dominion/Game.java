e3ccc026c31c2e2ba93867c101a0e48b0de2d03f
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2016-04-15 15:31:47 +0200

    Added treasure and victory cards to game
    Also hentai

--- Model/src/dominion/Game.java
+++ Model/src/dominion/Game.java
@@ -1,25 +1,24 @@
 package dominion;
 
-import java.util.ArrayList;
-
 /**
  * Created by Digaly on 23/03/2016.
  */
 public class Game
 {
-    private String cardSetName;
-    private Card[] cardSet;
+    private Card[] fixedCards;
+    private String kingdomCardSet;
+    private Card[] kingdomCards;
     private Player currentPlayer;
     private Player[] players;
     private int phase;
     private boolean isOver;
     private GameEngine gameEngine;
 
-    public Game(Account[] accounts, String cardSetName, GameEngine gameEngine)
+    public Game(Account[] accounts, String kingdomCardSet, GameEngine gameEngine)
     {
-        this.cardSetName = cardSetName;
+        this.kingdomCardSet = kingdomCardSet;
         this.gameEngine = gameEngine;
-        this.cardSet = cardSet(cardSetName);
+        this.kingdomCards = cardSet(kingdomCardSet);
         players = new Player[accounts.length];
 
         for (int i = 0; i < accounts.length; i++)
@@ -38,7 +37,7 @@ public class Game
         Card[] cardSet = new Card[10];
         if (name.equals("default"))
         {
-            cardSetNames = new String[] {"cellar", "market", "militia", "mine", "moat", "remodel", "smithy", "village", "woodcutter", "workshop", };
+            cardSetNames = new String[]{"cellar", "market", "militia", "mine", "moat", "remodel", "smithy", "village", "woodcutter", "workshop",};
         }
 
         for (int i = 0; i < cardSetNames.length; i++)
@@ -48,5 +47,38 @@ public class Game
             cardSet[i].setAmount(10);
         }
         return cardSet;
+    }
+
+    private Card[] fixedCards()
+    {
+        Card[] fixedCards = new Card[7];
+
+        fixedCards[0] = new Card(gameEngine.findCard("province"));
+        fixedCards[1] = new Card(gameEngine.findCard("duchy"));
+        fixedCards[2] = new Card(gameEngine.findCard("estate"));
+        fixedCards[3] = new Card(gameEngine.findCard("curse"));
+        fixedCards[4] = new Card(gameEngine.findCard("gold"));
+        fixedCards[5] = new Card(gameEngine.findCard("silver"));
+        fixedCards[6] = new Card(gameEngine.findCard("copper"));
+
+
+        if (players.length == 2)
+        {
+            fixedCards[0].setAmount(8);
+            fixedCards[1].setAmount(8);
+            fixedCards[2].setAmount(8);
+        } else
+        {
+            fixedCards[0].setAmount(12);
+            fixedCards[1].setAmount(12);
+            fixedCards[2].setAmount(12);
+        }
+
+        fixedCards[3].setAmount(players.length * 10 - 10);
+        fixedCards[4].setAmount(30);
+        fixedCards[5].setAmount(40);
+        fixedCards[6].setAmount(60 - players.length * 7);
+
+        return fixedCards;
     }
 }
