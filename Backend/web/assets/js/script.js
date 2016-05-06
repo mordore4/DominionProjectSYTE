164fe03f8c9e65447a40fa883753cf207efff62e
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2016-05-06 23:57:38 +0200

    Refactored web
    - Massively cleaned up javascript
    - New icons
    - Velvet background in game
    - Fixed cardscomecenter to actually be centered
    - Fixed favicon not working
    - Fixed menu "jumping" when loading page cause of hide() taking too
    long,
    - Cards themselves were moved to a separate folder
    - The script before refactoring is available in script_old.js
    - Hentai

--- Backend/web/assets/js/script.js
+++ Backend/web/assets/js/script.js
@@ -1,206 +1,77 @@
-/**
- * Created by Yannis on 1-12-2015.
- */
-
-var active = false;
-//var kaarten = ["Adventurer", "Bureaucrat", "Cellar", "Chancellor", "Chapel", "Copper", "Council_Room", "Curse", "Duchy", "Estate"];
-var kaarten = ["Adventurer", "Bureaucrat", "Cellar", "Chancellor", "Chapel", "Copper", "Council_Room"];
-var coinsTop = ["Coppertop", "Silvertop", "Goldtop", "Cursetop"];
-var StatesTop = ["provinceTop", "dutchyTop", "estateTop"];
+var tempHand = ["Adventurer", "Bureaucrat", "Cellar", "Chancellor", "Chapel", "Copper", "Council_Room"];
 var kingdomCards = ["Militia", "Remodel", "Smithy", "Market", "Mine", "Cellar", "Moat", "Village", "Woodcutter", "Workshop"];
-
-var huidigeAfbeelding = 1;
-var huidigeAfbeeldingTop = 1;
-
-var toevoegenAfbeeldingen = function () {
-    var xoffset = 0;
-    var angle = 0;
-
-    for (var i = 0, len = kaarten.length; i < len; i++) {
-        angle = i * 4;
-
-        var imageSource = "images/" + kaarten[i] + ".jpg";
-        /*var html = '<li style="left: ' + xoffset + 'px; z-index: ' + (kaarten.length - i) + '; transform: rotate(' + angle + 'deg) translate(0px , ' + (angle * 5) + 'px)">';*/
-        /*var html = '<li style="left: ' + xoffset + 'px; z-index: ' + (kaarten.length - i) + ';">';*/
-        var html = '<li style="background-image: url(' + imageSource + ');">';
-
-
-        //html += '<figure><img alt="' + kaarten[i] + '" title="' + kaarten[i] + '" src="' + imageSource + '" />';
-        /*html += '<figcaption>' + kaarten[i] + '</figcaption></figure></li>';*/
-
-        $('#hand').append(html);
-
-        xoffset += 80;
-    }
-
-};
-
-
-var toevoegenAfbeeldingenTop = function () {
-    for (var i = 0, len = coinsTop.length; i < len; i++) {
-
-        var imgsrc = "images/" + coinsTop[i] + ".png";
-
-        var html = '<li>';
-        html += '<figure><img alt="' + coinsTop[i] + '" title="' + coinsTop[i] + '" src="' + imgsrc + '" />';
-        /*html += '<figcaption>' + coinsTop[i] + '</figcaption></figure></li>'*/
-
-        $('#coins').append(html);
-    }
-    $('#coins li:first').show();
-};
-
-var toevoegenStatesTop = function () {
-    for (var i = 0, len = StatesTop.length; i < len; i++) {
-
-        var imgsrc = "images/" + StatesTop[i] + ".png";
-
-        var html = '<li>';
-        html += '<figure><img alt="' + StatesTop[i] + '" title="' + StatesTop[i] + '" src="' + imgsrc + '" />';
-        /*html += '<figcaption>' + coinsTop[i] + '</figcaption></figure></li>'*/
-
-        $('#topstates').append(html);
-    }
-    $('#topstates li:first').show();
-};
-
-var toevoegenKingdomCards = function () {
-
-    for (var i = 0, len = kingdomCards.length; i < len; i++) {
-        var cardName = kingdomCards[i];
-
-        var html = '<li>';
-        html += '<div class="kingdomcard">';
-        html += '<div class="kingdomcard-top" style="background-image: url(images/' + cardName + '.jpg);"></div>';
-        html += '<div class="kingdomcard-bottom" style="background-image: url(images/' + cardName + '.jpg);"></div>';
-        html += '</div>';
-        html += '</li>';
-
-        $("#kingdomcards ul").append(html);
-    }
-
-
-};
+var fixedCards = ["copper", "silver", "gold", "curse", "province", "duchy", "estate"];
 
 $(document).ready(function () {
-    console.log("De verbinding werkt");
-    $('#play').on('click', playGame);
-    toevoegenAfbeeldingen();
-    $('#hand').hide();
-    $('#coins').hide();
-    $('#topstates').hide();
-    $('#cardsComeCenter').hide();
-    $("#gamewindow").hide();
-    $("#form").hide();
-    $('#hand li').on('click', function () {
-        $(this).find('img').css('top', '100px');
-        //$(this).css('height', '180');
-
-    });
-
-    $("label[for=state_id]").parent().load("assets/fragments/states.html");
-
-    $(".register").on('click', function(){
+    $(".register").on('click', function () {
         $('#menu').hide();
         $("#form").show();
-
     });
-
-    /*$('#hand li').on('mouseenter', function () {
-     $(this).addClass("hoveredcard");
-     });
-
-     $('#hand li').on('mouseleave', function () {
-     $(this).removeClass("hoveredcard");
-     });*/
 
     $("#hand").sortable({
         revert: true,
         connectWith: "#cardsComeCenter"
-        //axis: "x"
     });
 
     $('#cardsComeCenter').sortable({
         handle: "none",
         placeholder: false,
         items: "li:not('#cardsComeCenter li')"
-        /*placeholder: "placeholder"*/
     });
 
-    /*$('#hand li').draggable({
-     revert: true,
-     zIndex: 1000,
-     revertDuration: 500,
-     connectToSortable: "#sortable",
-     scroll: false,
-     start: function () {
-     $(this).addClass("hoveredcard");
-     },
-     stop: function() {
-     $(this).removeClass("hoveredcard");
-     }
-     });*/
-    toevoegenAfbeeldingenTop();
-    toevoegenStatesTop();
-    toevoegenKingdomCards();
-
-    /*objectDragen();*/
+    $("label[for=state_id]").parent().load("assets/fragments/states.html");
+    $('#play').on('click', playGame);
 });
-
-
-$body = $("body");
-
-$(document).on({
-    ajaxStart: function () {
-        if (active) $body.addClass("loading");
-    },
-    ajaxStop: function () {
-        if (active) setTimeout(stopLoading, 2000);
-        active = false;
-    }
-});
-
-var stopLoading = function () {
-    $body.removeClass("loading");
-};
 
 var playGame = function (e) {
-    $('#coins').show();
-    $('#topstates').show();
-    $("#gamewindow").show();
-    $('#cardsComeCenter').show();
-    //$('body').css({background : 'url(images/daszeker.jpg) no-repeat '});
     e.preventDefault();
 
-    /*active = true;*/
+    addFixedCards();
+    addKingdomCards(kingdomCards);
+    createHand(tempHand);
 
-    $.ajax({
-        url: "index.html",
-        method: "GET",
-        data: "",
-        dataType: "html",
-        success: function (data) {
-            console.log(data);
-        }
-    });
+    $('body').addClass("game");
 
     $('#menu').hide();
-    $('#hand').show();
     $("div.mastfoot").hide();
 
-    /* var goTo = this.getAttribute("href");
-
-     window.location = goTo;
-
-
-     setTimeout(function () {
-     window.location = goTo;
-     }, 3000);*/
+    $("#gamewindow").show();
+    $('#cardsComeCenter').show();
 };
 
-/*var objectDragen = function(){
- $("#cards li").draggable();
+var createHand = function (cardsArray) {
+    for (var i = 0, len = cardsArray.length; i < len; i++) {
+        var imageSource = "images/cards/" + cardsArray[i] + ".jpg";
+        var html = '<li style="background-image: url(' + imageSource + ');">';
 
- }
- */
+        $('#hand').append(html);
+    }
+};
 
+var addFixedCards = function () {
+    for (var i = 0, len = fixedCards.length; i < len; i++) {
+        var imgsrc = "images/" + fixedCards[i] + "_top.png";
+
+        var html = '<li><figure>' +
+            '<img alt="' + fixedCards[i] + '" title="' + fixedCards[i] + '" src="' + imgsrc + '" />' +
+            '</figure></li>';
+
+        if (i > 3)
+            $('#topstates').append(html);
+        else
+            $('#coins').append(html);
+    }
+};
+
+var addKingdomCards = function (cardsArray) {
+    for (var i = 0, len = cardsArray.length; i < len; i++) {
+        var cardName = cardsArray[i];
+
+        var html = '<li><div class="kingdomcard">';
+        html += '<div class="kingdomcard-top" style="background-image: url(images/cards/' + cardName + '.jpg);"></div>';
+        html += '<div class="kingdomcard-bottom" style="background-image: url(images/cards/' + cardName + '.jpg);"></div>';
+        html += '</div></li>';
+
+        $("#kingdomcards").find('ul').append(html);
+    }
+};
