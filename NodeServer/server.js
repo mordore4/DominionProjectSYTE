426d2a700dd0362039f48d8002108b57042ab52b
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2016-05-13 15:37:16 +0200

    Added basic node + socket.io
    Hentai

---
+++ NodeServer/server.js
@@ -0,0 +1,24 @@
+var http = require('http').Server();
+var io = require('socket.io')(http);
+var port = 443;
+
+http.listen(port, function(){
+	console.log('Dominion-chatserver running on *:' + port);
+	console.log(getColor(31) + "\\~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~/");
+	console.log(getColor(32) + "|                                                     |");
+	console.log(getColor(36) + "|  DDD    OOO   M   M  III  N   N  III   OOO   N   N  |");
+	console.log(getColor(34) + "|  D  D  O   O  MM MM   I   NN  N   I   O   O  NN  N  |");
+	console.log(getColor(35) + "|  D  D  O   O  M M M   I   N N N   I   O   O  N N N  |");
+	console.log(getColor(31) + "|  D  D  O   O  M   M   I   N  NN   I   O   O  N  NN  |");
+	console.log(getColor(32) + "|  DDD    OOO   M   M  III  N   N  III   OOO   N   N  |");
+	console.log(getColor(36) + "|                                                     |");
+	console.log(getColor(34) + "/~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~\\");
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+});
+
+var getColor = function(number) {
+	return String.fromCharCode(27) + '[' + number + 'm';
+};
